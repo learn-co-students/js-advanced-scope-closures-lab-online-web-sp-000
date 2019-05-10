5fc45bceb5417fc8ce7bd75ea38c209db142f082
github username <github email address>
2019-05-10 17:30:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function produceDrivingRange(blockRange){
   return function(range1, range2) {
-    let distance = range2 - range1;
-   if(range2 - range1 <= 10){
-    return `within range by  ${blockRange - (10 - distance)}`;
+    let distance = parseInt(range2) - parseInt(range1);
+    let absDis = Math.abs(distance);
+   if(blockRange - absDis > 0){
+    return `within range by ${blockRange - absDis}`;
    }
-    return `${blockRange - (10 - distance)} blocks out of range`;
+    return `${Math.abs(blockRange - absDis)} blocks out of range`;
  };
     // return eightBlockRange(st1, st2){
     //
     // };
     //
+}
+
+function produceTipCalculator(tip){
+  return function(num) {
+    return num * tip;
+ };
+  //   return `${Math.abs(blockRange - absDis)} blocks out of range`;
+  // };
+}
+
+function createDriver(){
+    let driverId = 0;
+
+  return class {
+    constructor(name){
+       this.id = ++driverId;
+       this.name = name;
+    }
+  }
 }
